1a979775f4f20c8cddb1436fb2f82ab8429aed42
Anastasia Yakovleva <146084202+r1ghtang@users.noreply.github.com>
2026-04-09 08:58:57 +0500

    Create Stroka.h

---
+++ lab6/task3/Stroka.h
@@ -0,0 +1,31 @@
+#ifndef STROKA_H
+#define STROKA_H
+
+#include <iostream>
+using namespace std;
+
+class Stroka {
+private:
+    char str[80];
+
+    // Свои служебные функции
+    int myLen(const char* s) const;
+    void myCopy(char* dest, const char* src);
+    int myCompare(const char* s1, const char* s2) const;
+    void myConcat(char* dest, const char* src);
+
+public:
+    Stroka();
+    Stroka(const char* s);
+    Stroka(const Stroka& s);
+
+    Stroka& operator=(const Stroka& s);
+    Stroka operator+(const Stroka& s) const;
+    bool operator==(const Stroka& s) const;
+
+    int dlina() const;
+    void vvod();
+    void vyvod() const;
+};
+
+#endif
